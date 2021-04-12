751270bd5e9ab94370695d6dbeac9b6cc557a03f
klol <adg08101@gmail.com>
2021-04-12 17:20:38 -0400

    Pandas
    Pandas lib testing

--- Pandas1.py
+++ Pandas1.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as panda
+
 
 def main():
     # DataFrame desde diccionario
@@ -9,14 +11,34 @@ def main():
 
     tabla = panda.DataFrame(data=personas)
 
-    print(tabla)
+    # print(tabla)
 
     # DataFrame desde Lista de Listas
     data = [['monica', 10], ['maria', 15], ['marieta', 25]]
-    columns= ['nombre', 'edad']
+    columns = ['nombre', 'edad']
 
     datos = panda.DataFrame(data=data, columns=columns)
-    print(datos)
+    # print(datos)
+
+    # Desde Lista de Diccionarios
+
+    data = [
+        {'name': 'Abdul',
+         'age': 115},
+        {'name': 'Habat',
+         'age': 231},
+        {'name': 'Mamet',
+         'age': 222},
+        {'name': 'Mumot',
+         'age': 412},
+    ]
+
+    df = panda.DataFrame(data=data)
+
+    # print(df)
+
+    df = panda.DataFrame(np.random.randn(4, 6), columns=['a', 'b', 'c', 'd', 'e', 'f'])
+    print(df)
 
 if __name__ == '__main__':
     main()
