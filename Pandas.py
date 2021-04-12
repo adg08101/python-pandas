c6d7fd799cae7c40d2511d8f0863468ef5e7c217
klol <adg08101@gmail.com>
2021-04-12 14:07:48 -0400

    Test
    Panda Test

--- Pandas.py
+++ Pandas.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+
+def main():
+    l = ['Mate', 'Fisics', 'Programming']
+    d = {1: 'Yo', 2: 'Tu', 3: 'El', 4: 'Ellos', 5: 'Nosotros'}
+    sl = pd.Series(data=l, dtype='string')
+    dl = pd.Series(data=d)
+    print(sl)
+    print(dl)
+
+
+if __name__ == '__main__':
+    main()
