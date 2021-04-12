2e16610af6d33d413bca8f5e9f0b30c7fa5d3d82
klol <adg08101@gmail.com>
2021-04-12 14:19:02 -0400

    Testing
    Testing Pandas lib

--- Pandas.py
+++ Pandas.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+def view(s):
+    print(s)
+    print('Items:', s.size, 'Index:', s.index,'Type:', s.dtype)
+    print('\n')
 
 def main():
     l = ['Mate', 'Fisics', 'Programming']
     d = {1: 'Yo', 2: 'Tu', 3: 'El', 4: 'Ellos', 5: 'Nosotros'}
-    sl = pd.Series(data=l, dtype='string')
+    sl = pd.Series(data=l)
     dl = pd.Series(data=d)
-    print(sl)
-    print(dl)
+    view(sl)
+    view(dl)
 
 
 if __name__ == '__main__':
