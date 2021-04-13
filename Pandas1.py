ed05f5140a87d743a33c73036c5d8aa91c946e35
klol <adg08101@gmail.com>
2021-04-13 14:16:37 -0400

    Pandas
    Pandas lib testing DataFrames

--- Pandas1.py
+++ Pandas1.py
@@ -35,10 +35,33 @@ def main():
 
     df = panda.DataFrame(data=data)
 
-    # print(df)
-
     df = panda.DataFrame(np.random.randn(4, 6), columns=['a', 'b', 'c', 'd', 'e', 'f'])
-    print(df)
+    df.to_excel("output.xlsx", sheet_name='Sheet_name_1')
 
+    nf = panda.read_excel("output.xlsx", sheet_name='Sheet_name_1', index_col=0)
+    print(nf)
+    print(properties(nf))
+
+def properties(s = None):
+    l = {
+        'info()' : '''Devuelve información (número de filas, número de columnas, índices, tipo de las columnas y'
+                      memoria usado) sobre el DataFrame df.''',
+        'shape' : 'Devuelve una tupla con el número de filas y columnas del DataFrame df.',
+        'size' : 'Devuelve el número de elementos del DataFrame.',
+        'columns' : 'Devuelve una lista con los nombres de las columnas del DataFrame df.',
+        'index' : 'Devuelve una lista con los nombres de las filas del DataFrame df.',
+        'dtypes' : 'Devuelve una serie con los tipos de datos de las columnas del DataFrame df.',
+        # 'head(n)' : 'Devuelve las n primeras filas del DataFrame df.',
+        # 'tail(n)' : 'Devuelve las n últimas filas del DataFrame df.'
+    }
+
+    i = 1
+
+    for k in l:
+        oper = 's.' + k
+        print(str(i) + '.', l[k], ': \nResulted\n'
+        if l[k].find('una serie') != -1 else ': Resulted\t', eval(oper))
+        i += 1
+        print('\n')
 if __name__ == '__main__':
     main()
